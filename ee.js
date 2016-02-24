509f35e5354565245b1547e5de65e1f6b32a72c0
Dima Gurkalenko <dmitrij.gurkalenko@gmail.com>
2016-02-24 13:25:51 +0200

    ee exercises

---
+++ ee.js
@@ -0,0 +1,15 @@
+var EventEmitter = require('events').EventEmitter;
+
+var server = new EventEmitter;
+
+server.on('request', function(request) {
+  request.approved = true;
+});
+
+server.on('request', function(request) {
+  console.log(request);
+});
+
+server.emit('request', {from: 'Клиент'});
+
+server.emit('request', {from: 'Еще клиент'});
